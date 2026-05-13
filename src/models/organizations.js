840d1eff8deb4d7f682ef1f78a9385622b30751b
Jgallardo613 <Jgallardo1@byupathway.edu>
2026-05-13 17:38:49 -0600

    organizations.js

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -2,11 +2,14 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }  // ADD THIS LINE
+});
 
 export const getAllOrganizations = async () => {
   try {
-    const result = await pool.query('SELECT * FROM organization');
+    const result = await pool.query('SELECT * FROM organization ORDER BY name');
     return result.rows;
   } catch (err) {
     console.error('Error fetching organizations:', err);
